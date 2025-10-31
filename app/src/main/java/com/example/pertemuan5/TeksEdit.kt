5c64f68e3d7c371fb28ff97b0cc50e03f7ab7213
SandyArdiyan <sanardiyan881@gmail.com>
2025-10-31 15:25:17 +0700

    Membuat Colomn Alamat

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -66,3 +66,13 @@ fun FormDataDiri(modifier: Modifier
                             })
                         Text(text = item)
                     }
+                OutlinedTextField(
+                    value = textAlamat,
+                    singleLine = true,
+                    modifier = Modifier.width(width = 250.dp),
+                    label = { Text(text = "Alamat Lengkap") },
+                    onValueChange = {
+                        textAlamat = it
+                    }
+                }
+
