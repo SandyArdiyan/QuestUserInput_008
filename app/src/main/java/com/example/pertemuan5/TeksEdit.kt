3fb5c7e8eba1c2e0695729030895902dfdea98fa
SandyArdiyan <sanardiyan881@gmail.com>
2025-10-31 15:24:54 +0700

    Membuat Colomn Gender

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -51,3 +51,18 @@ fun FormDataDiri(modifier: Modifier
             onValueChange = {
                 textNama = it
             }
+        )
+        Row {
+            gender.forEach { item ->
+                    Row(
+                        modifier = Modifier.selectable(
+                            selected = textJK == item,
+                            onClick = { textJK = item }
+                        ), verticalAlignment = Alignment.CenterVertically) {
+                        RadioButton(
+                            selected = textJK == item,
+                            onClick = {
+                                textJK = item
+                            })
+                        Text(text = item)
+                    }
